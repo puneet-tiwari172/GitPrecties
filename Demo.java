792a944405bf5344c33b07d4bbf4a309078ef9e7
puneet tiwari <tiwaripuneet133@gmail.com>
2025-11-17 15:47:46 +0530

    after changes

--- Demo.java
+++ Demo.java
@@ -3,6 +3,9 @@ public static void main(String args[]){
    System.out.println("Demo is success fully ");
 
 
-}    
+}  
+public static void printMessage(){
+   System.out.println("This is a message from the printMessage method.");  
 
+}
 }
